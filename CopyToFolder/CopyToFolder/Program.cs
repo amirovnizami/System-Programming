5ee654e8a9746b58b9265dc8b7c91557e795a9dc
Nizami Amirov <amirovnizami395@gmail.com>
2024-09-18 11:07:46 +0400

    CopyToFolder

---
+++ CopyToFolder/CopyToFolder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+namespace CopyToFolder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter File Path : ");
+            var filePath = Console.ReadLine();
+            Console.Write("Enter Folder Path : ");
+            var folderPath = Console.ReadLine();
+            void CopyFile()
+            {
+                try
+                {
+                    if (File.Exists(filePath))
+                    {
+                        var content = File.ReadAllBytes(filePath);
+                        string destinationFilePath = Path.Combine(folderPath, Path.GetFileName(filePath));
+
+                        for (int i = content.Length; i >= 0; i--)
+                        {
+                            Console.WriteLine($"{i} / {content.Length}");
+                            Thread.Sleep(0);
+                            Console.Clear();
+
+                        }
+                        File.Copy(filePath, destinationFilePath, true);
+
+                        Console.WriteLine("Kopyalama ugurla tamamlandi..");
+
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            Thread thread = new Thread(CopyFile);
+            thread.Start();
+            
+        }
+    }
+}
